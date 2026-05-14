0ed6414e2e2697bf3e9195e0f58da60838e92ce7
Alexander Foremny <aforemny@posteo.de>
2026-05-14 16:48:05 +0200

    add btop

--- src/defaults.nix
+++ src/defaults.nix
@@ -1,9 +1,11 @@
 { config, ... }:
 {
   nixosModules.defaults =
-    { lib, ... }:
+    { lib, pkgs, ... }:
     {
       boot.zfs.forceImportRoot = false;
+      environment.systemPackages = with pkgs; [ btop ];
+      networking.networkmanager.enable = true;
       networking.useNetworkd = true;
       services.resolved.enable = true;
       users.mutableUsers = false;
