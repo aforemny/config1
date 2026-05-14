af5b37e8f9e13dec5072f0adbed1602f7b55e87d
Alexander Foremny <aforemny@posteo.de>
2026-05-14 16:35:41 +0200

    refactor asecret

--- src/asecret.nix
+++ src/asecret.nix
@@ -12,23 +12,36 @@ in
     type = lib.types.path;
   };
   config = {
-    nixosModules.asecret = "${/home/aforemny/s/asecret}/modules"; # TODO
-    overlays = {
-      asecret = import "${/home/aforemny/s/asecret}/pkgs"; # TODO
-      nix-plugins =
-        self: super:
-        let
-          major = "2";
-          minor = "30";
-        in
-        {
-          nix = super.nixVersions."nix_${major}_${minor}"; # TODO
-          nix-plugins = (
-            super.nix-plugins.override {
-              nixComponents = super.nixVersions."nixComponents_${major}_${minor}";
-            }
-          );
+    nixosModules.asecret =
+      { lib, options, ... }:
+      {
+        imports = [
+          "${/home/aforemny/s/asecret}/modules" # TODO
+        ];
+        config = lib.mkIf (options ? "state") {
+          state.directories = [ "/var/src/secrets" ];
         };
+      };
+    overlays = {
+      asecret = lib.composeManyExtensions [
+        (import "${/home/aforemny/s/asecret}/pkgs" # TODO
+        )
+        (
+          self: super:
+          let
+            major = "2";
+            minor = "30";
+          in
+          {
+            nix = super.nixVersions."nix_${major}_${minor}"; # TODO
+            nix-plugins = (
+              super.nix-plugins.override {
+                nixComponents = super.nixVersions."nixComponents_${major}_${minor}";
+              }
+            );
+          }
+        )
+      ];
     };
     devShell = {
       packages = with pkgs; [
@@ -43,5 +56,8 @@ in
         '
       '';
     };
+    _cake.cake-cli.cake-deploy.postCopyClosure = ''
+      ASECRET_OUT=$target1:/var/src/secrets asecret export
+    '';
   };
 }
