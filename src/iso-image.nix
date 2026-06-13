4cfd95b7116789f262bda56e627003a5cc54bc1e
Alexander Foremny <aforemny@posteo.de>
2026-06-13 14:10:10 +0200

    isoImages/tower: init (broken)

--- src/iso-image.nix
+++ src/iso-image.nix
@@ -11,6 +11,24 @@
     default = { };
   };
   # TODO `systems.m1.config.system.build.isoImage`
+  config.isoImages.tower = import "${pkgs.path}/nixos/lib/eval-config.nix" {
+    modules = [
+      #"${sources.disko}/module.nix"
+      #"${sources.nixos-apple-silicon}/apple-silicon-support"
+      "${sources.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+      #config.platforms.m1
+      (
+        #  #{ lib, ... }:
+        {
+          #    disabledModules = [
+          #      "${sources.nixos-images}/nix/latest-zfs-kernel.nix"
+          #    ];
+          #    #system.stateVersion = lib.mkForce lib.trivial.release;
+          #hardware.asahi.peripheralFirmwareDirectory = ./platforms/m1;
+        })
+    ];
+    #system = "aarch64-linux";
+  };
   config.isoImages.m1 = import "${pkgs.path}/nixos/lib/eval-config.nix" {
     modules = [
       #"${sources.disko}/module.nix"
