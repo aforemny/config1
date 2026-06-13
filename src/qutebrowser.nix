ba79d0a9e9ab9bfe4724a07cddd70c98e7cc2516
Alexander Foremny <aforemny@posteo.de>
2026-06-13 21:08:05 +0200

    qutebrowser: gate behind `graphical` tag

--- src/qutebrowser.nix
+++ src/qutebrowser.nix
@@ -55,79 +55,81 @@
           }
         )
       ];
-      config = lib.mkMerge [
-        {
-          programs.qutebrowser = {
-            enable = true;
-            package = pkgs.qutebrowser.override {
-              enableWideVine = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
-            };
-            settings = {
-              fonts = {
-                default_family = "IosevkaTerm Nerd Font";
-                #default_size = "28px";
-                #web.size = {
-                #  default = 16;
-                #  default_fixed = 16;
-                #};
+      config = lib.mkIf (config.tags.graphical or false) (
+        lib.mkMerge [
+          {
+            programs.qutebrowser = {
+              enable = true;
+              package = pkgs.qutebrowser.override {
+                enableWideVine = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
               };
-              #qt.highdpi = false;
-              tabs.tabs_are_windows = true;
+              settings = {
+                fonts = {
+                  default_family = "IosevkaTerm Nerd Font";
+                  #default_size = "28px";
+                  #web.size = {
+                  #  default = 16;
+                  #  default_fixed = 16;
+                  #};
+                };
+                #qt.highdpi = false;
+                tabs.tabs_are_windows = true;
+              };
             };
-          };
-          state.directories = [ ".local/share/qutebrowser" ];
-        }
-        {
-          programs.qutebrowser.searchEngines = {
-            g = "https://github.com/search?q={}";
-          };
-        }
-        {
-          programs.qutebrowser.keyBindings = {
-            normal = {
-              "<Ctrl-v>" = "spawn mpv {url}";
-              ",l" = ''config-cycle spellcheck.languages ["de-DE"] ["en-US"]'';
-              "<F1>" = lib.mkMerge [
-                "config-cycle tabs.show never always"
-                "config-cycle statusbar.show in-mode always"
-                "config-cycle scrolling.bar never always"
-              ];
+            state.directories = [ ".local/share/qutebrowser" ];
+          }
+          {
+            programs.qutebrowser.searchEngines = {
+              g = "https://github.com/search?q={}";
             };
-            prompt = {
-              "<Ctrl-y>" = "prompt-yes";
+          }
+          {
+            programs.qutebrowser.keyBindings = {
+              normal = {
+                "<Ctrl-v>" = "spawn mpv {url}";
+                ",l" = ''config-cycle spellcheck.languages ["de-DE"] ["en-US"]'';
+                "<F1>" = lib.mkMerge [
+                  "config-cycle tabs.show never always"
+                  "config-cycle statusbar.show in-mode always"
+                  "config-cycle scrolling.bar never always"
+                ];
+              };
+              prompt = {
+                "<Ctrl-y>" = "prompt-yes";
+              };
             };
-          };
-        }
-        {
-          programs.qutebrowser = {
-            userScripts.qute-pass.enable = true;
-            keyBindings.normal = {
-              ",p" =
-                "spawn --userscript qute-pass --username-pattern '[uU]sername: ?(.*)' --username-target secret";
-              ",up" = "spawn --userscript qute-pass --username-only";
-              ",pp" = "spawn --userscript qute-pass --password-only";
-              ",op" = "spawn --userscript qute-pass --otp-only";
+          }
+          {
+            programs.qutebrowser = {
+              userScripts.qute-pass.enable = true;
+              keyBindings.normal = {
+                ",p" =
+                  "spawn --userscript qute-pass --username-pattern '[uU]sername: ?(.*)' --username-target secret";
+                ",up" = "spawn --userscript qute-pass --username-only";
+                ",pp" = "spawn --userscript qute-pass --password-only";
+                ",op" = "spawn --userscript qute-pass --otp-only";
+              };
             };
-          };
-          programs.rofi.enable = true;
-        }
-        # TODO colorscheme
-        #{
-        #  programs.qutebrowser.extraConfig = builtins.readFile (
-        #    pkgs.runCommand "config.py"
-        #      (lib.mapAttrs' (name: lib.nameValuePair "${name}_hex") config.colorscheme.colors)
-        #      ''
-        #        cat ${
-        #          pkgs.fetchurl {
-        #            url = "https://raw.githubusercontent.com/tinted-theming/base16-qutebrowser/9e16ab2c6f090197341eec2d65faae4803928153/templates/default.mustache";
-        #            hash = "sha256-l8dNIg0D97f9W+NcaMYxxi8kxEIsg/1zpztlyWvS1WY=";
-        #          }
-        #        } |
-        #        sed 's/{{base\(..\)-hex}}/{{base\1_hex}}/g' |
-        #        ${lib.getExe pkgs.mo} >$out
-        #      ''
-        #  );
-        #}
-      ];
+            programs.rofi.enable = true;
+          }
+          # TODO colorscheme
+          #{
+          #  programs.qutebrowser.extraConfig = builtins.readFile (
+          #    pkgs.runCommand "config.py"
+          #      (lib.mapAttrs' (name: lib.nameValuePair "${name}_hex") config.colorscheme.colors)
+          #      ''
+          #        cat ${
+          #          pkgs.fetchurl {
+          #            url = "https://raw.githubusercontent.com/tinted-theming/base16-qutebrowser/9e16ab2c6f090197341eec2d65faae4803928153/templates/default.mustache";
+          #            hash = "sha256-l8dNIg0D97f9W+NcaMYxxi8kxEIsg/1zpztlyWvS1WY=";
+          #          }
+          #        } |
+          #        sed 's/{{base\(..\)-hex}}/{{base\1_hex}}/g' |
+          #        ${lib.getExe pkgs.mo} >$out
+          #      ''
+          #  );
+          #}
+        ]
+      );
     };
 }
