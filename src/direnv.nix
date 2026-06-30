4351a30386a9e0e24bcea9985dc981080caf32a5
Alexander Foremny <aforemny@posteo.de>
2026-06-30 19:25:16 +0200

    direnv: persist

--- src/direnv.nix
+++ src/direnv.nix
@@ -4,4 +4,9 @@
     lib.mkIf config.tags.graphical {
       programs.direnv.enable = true;
     };
+  homeManagerModules.direnv =
+    { config, lib, ... }:
+    lib.mkIf config.tags.graphical {
+      state.directories = [ ".local/share/direnv" ];
+    };
 }
