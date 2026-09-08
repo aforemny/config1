5ef3326e0e7afb11e8dcf2c7b55a64309cc973c4
Alexander Foremny <aforemny@posteo.de>
2026-09-08 16:56:41 +0200

    yubikey: drop

--- src/yubikey.nix
+++
@@ -1,9 +0,0 @@
-{
-  nixosModules.yubikey = { lib, ... }: {
-    programs.yubikey-manager.enable = true;
-    programs.yubikey-touch-detector.enable = true;
-    security.pam.u2f.enable = true;
-    services.yubikey-agent.enable = true;
-    services.fprintd.enable = lib.mkForce false; # TODO
-  };
-}
